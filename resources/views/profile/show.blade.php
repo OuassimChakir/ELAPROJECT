4e2799ac78a638a17608e99690e6679512344e27
Vaxim <vaxim.edengarde@gmail.com>
2022-10-18 20:37:59 +0100

    master

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,4 +1,51 @@
-<x-app-layout>
+
+@extends('layouts.layout')
+@section('title')
+    Profil
+@endsection
+@section('style')
+    <!-- Scripts -->
+    <link rel="stylesheet" href="/css/app.css">
+    <script src="/js/app.js"></script>
+    <!-- Styles -->
+    @livewireStyles
+@endsection
+@section('content')
+@if (session()->has('successMessage'))
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+    {{session()->get('successMessage')}}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+@elseif(session()->has('deleteMessage'))
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    {{session()->get('deleteMessage')}}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+@elseif(session()->has('updateMessage'))
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+    {{session()->get('updateMessage')}}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+@endif
+  <!--errour du validation -->
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+  <!-- end errour du validation -->
+<div class="breadcrumb-wrapper breadcrumb-contacts">
+    <div>
+        <h1>Liste des Utilisateurs</h1>
+        <p class="breadcrumbs"><span><a href="{{route('acceuil')}}">Acceuil</a></span>
+        <span><i class="mdi mdi-chevron-right"></i></span>Profil
+        </p>
+    </div>
+<div>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Profile') }}
@@ -6,7 +53,7 @@
     </x-slot>
 
     <div>
-        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto py-10 sm:px-10">
             @if (Laravel\Fortify\Features::canUpdateProfileInformation())
                 @livewire('profile.update-profile-information-form')
 
@@ -32,14 +79,8 @@
             <div class="mt-10 sm:mt-0">
                 @livewire('profile.logout-other-browser-sessions-form')
             </div>
-
-            @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())
-                <x-jet-section-border />
-
-                <div class="mt-10 sm:mt-0">
-                    @livewire('profile.delete-user-form')
-                </div>
-            @endif
         </div>
     </div>
-</x-app-layout>
+<script src="{{asset('JS/jquery.min.js')}}"></script>
+
+@endsection
