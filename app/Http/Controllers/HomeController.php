491578a17612e554f41eaf0c1827ef11d5382812
Vaxim08 <vaxim.edengarde@gmail.com>
2023-09-04 14:41:28 +0100

    master

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,6 +24,7 @@ class HomeController extends Controller
      
 
     public function index(){
+        dd('Hello');
         // ---------------- Année Scolaire ----------- //
         //dd(session()->get("user"));
         if(!Storage::exists('anneeScolaire.txt')){
