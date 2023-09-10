773e28d354dff4782a7fe772e5011dd4043ae4d2
Vaxim08 <vaxim.edengarde@gmail.com>
2023-09-10 15:48:37 +0100

    newmaster

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,6 +22,7 @@ class HomeController extends Controller
      
 
     public function index(){
+        dd(session()->all());
         // ---------------- Année Scolaire ----------- //
         //dd(session()->get("user"));
         if(!Storage::exists('anneeScolaire.txt')){
--- vendor/laravel/fortify/src/Http/Controllers/AuthenticatedSessionController.php
+++ vendor/laravel/fortify/src/Http/Controllers/AuthenticatedSessionController.php
@@ -2,10 +2,12 @@
 
 namespace Laravel\Fortify\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Contracts\Auth\StatefulGuard;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Routing\Pipeline;
+use Illuminate\Support\Facades\Session;
 use Laravel\Fortify\Actions\AttemptToAuthenticate;
 use Laravel\Fortify\Actions\EnsureLoginIsNotThrottled;
 use Laravel\Fortify\Actions\PrepareAuthenticatedSession;
@@ -55,7 +57,8 @@ class AuthenticatedSessionController extends Controller
      * @return mixed
      */
     public function store(LoginRequest $request)
-    {
+    {  
+        Session::put('user',User::getUser($request->username));
         return $this->loginPipeline($request)->then(function ($request) {
             return app(LoginResponse::class);
         });
