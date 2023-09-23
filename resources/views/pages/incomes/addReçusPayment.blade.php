4c2a714d06c00bdfc348aae55bd4b4d8f74e4fb1
OTMANMZOUGH <mzoughotman12@gmail.com>
2023-09-23 02:09:18 +0200

    newmaster

--- resources/views/pages/incomes/addReçusPayment.blade.php
+++ resources/views/pages/incomes/addReçusPayment.blade.php
@@ -82,9 +82,9 @@
                                 </div>
                             </div>
                             <div class="col-lg-6">
-                                <div class="form-group mb-4">
+                                <div class="form-group mb-4 was-validated">
                                     <label for="datePayment">Date du Payement</label>
-                                    <input type="date" name="datePayment" class="form-control" id="datePayment"
+                                    <input type="date" name="datePayment" class="form-control " id="datePayment"
                                         value="{{ date('Y-m-d') }}" required>
                                 </div>
                             </div>
@@ -93,7 +93,7 @@
                         </div>
                         <div class="row mb-2 g-3">
                             <div class="col-lg-6">
-                                <div class="form-group mb-3">
+                                <div class="form-group mb-3 was-validated">
                                     <label for="paimentAmount">Montant à Payer</label>
                                     <input type="number" class="form-control" name="amount" id="paimentAmount"
                                         value="{{-- $paiment->amount --}}" required>
@@ -101,9 +101,9 @@
                                 </div>
                             </div>
                             <div class="col-lg-6">
-                                <div class="form-group mb-3">
+                                <div class="form-group mb-3 was-validated">
                                     <label for="paimentAmountPaid">Montant Payé</label>
-                                    <input type="number" class="form-control" name="amountPaid" id="paimentAmountPaid"
+                                    <input type="number" class="form-control "  name="amountPaid" id="paimentAmountPaid"
                                         max="{{-- $paiment->amount --}}" min="0" required>
                                     <small class="text-muted">Le montant payer par le client en Dirham</small>
                                 </div>
@@ -112,7 +112,7 @@
                         <div class="row mb-2 g-3">
                             <div class="col-lg-6">
                                 <label>Type de Paiement</label>
-                                <div class="col">
+                                <div class="col was-validated">
                                     <div class="form-check">
                                         <input class="form-check-input" value="Espece" type="radio"
                                             name="paymentMode" id="typePyament1" checked>
@@ -173,6 +173,8 @@
             });
             $('body').on('click', '#searchBtn', function() {
                 var value = $('#search').val();
+                   $('#search').removeClass("is-invalid");
+                    $('#search').addClass("is-valid");
                 // Make the second AJAX request
                 $.ajax({
                     url: "{{ route('search.group') }}",
@@ -198,6 +200,8 @@
                     }
                 });
             });
+            $('#search').removeClass("is-valid");
+            $('#search').addClass("is-invalid");
         });
 
     });
