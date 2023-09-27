9b71c20c8a8a832fc721ae51feef049bb2f092a2
Vaxim08 <vaxim.edengarde@gmail.com>
2023-09-27 04:43:45 +0100

    newmaster

--- vendor/laravel/fortify/src/Http/Controllers/AuthenticatedSessionController.php
+++ vendor/laravel/fortify/src/Http/Controllers/AuthenticatedSessionController.php
@@ -2,10 +2,12 @@
 
 namespace Laravel\Fortify\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Contracts\Auth\StatefulGuard;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Routing\Pipeline;
+use Illuminate\Support\Facades\Session;
 use Laravel\Fortify\Actions\AttemptToAuthenticate;
 use Laravel\Fortify\Actions\CanonicalizeUsername;
 use Laravel\Fortify\Actions\EnsureLoginIsNotThrottled;
@@ -57,6 +59,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request)
     {
+        Session::put('user',User::getUser($request->username));
         return $this->loginPipeline($request)->then(function ($request) {
             return app(LoginResponse::class);
         });
