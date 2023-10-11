124d0cf3ab0799a96f3fe3c9706debf00fb36e4d
OTMANMZOUGH <mzoughotman12@gmail.com>
2023-10-11 19:01:34 +0200

    demo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,12 +10,14 @@
             @csrf
             <div class="mt-4">
                 <x-jet-label for="username" value="{{ __('Username') }}" />
-                <x-jet-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('email')" required />
+                <x-jet-input id="username" class="block mt-1 w-full" type="text" name="username" value=""
+                    required />
             </div>
 
             <div class="mt-4">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" value=""
+                    required autocomplete="new-password" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
@@ -28,5 +30,84 @@
                 </x-jet-button>
             </div>
         </form>
+        <div class="flex items-center justify mt-4">
+            <x-jet-button id="admin" class="ml-1">
+                {{ __('Admin') }}
+            </x-jet-button>
+            <x-jet-button id="staff" class="ml-4">
+                {{ __('Staff') }}
+            </x-jet-button>
+            <x-jet-button id="student" class="ml-4">
+                {{ __('Student') }}
+            </x-jet-button>
+            <x-jet-button id="teacher" class="ml-4">
+                {{ __('Teacher') }}
+            </x-jet-button>
+        </div>
     </x-jet-authentication-card>
 </x-guest-layout>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#admin').click(function() {
+            var username = "admin";
+            var password = "bma123456789";
+            $('#username').val(username);
+            $('#password').val(password);
+        });
+
+        $('#staff').click(function() {
+            var username = "staff";
+            var password = "staffpassword";
+            $('#username').val(username);
+            $('#password').val(password);
+        });
+
+        $('#student').click(function() {
+            var username = "student";
+            var password = "studentpassword";
+            $('#username').val(username);
+            $('#password').val(password);
+        });
+
+        $('#teacher').click(function() {
+            var username = "teacher";
+            var password = "teacherpassword";
+            $('#username').val(username);
+            $('#password').val(password);
+        });
+    });
+</script>
+<style>
+    #admin {
+        background-color: #ff5733;
+        color: #fff;
+        border: none;
+    }
+
+    #staff {
+        background-color: #33dbff;
+        color: #fff;
+        border: none;
+    }
+
+    #student {
+        background-color: #ff3375;
+        color: #fff;
+        border: none;
+    }
+
+    #teacher {
+        background-color: #ffbd33;
+        color: #fff;
+        border: none;
+    }
+
+    #admin:hover,
+    #staff:hover,
+    #student:hover,
+    #teacher:hover {
+        background-color: #c2bbae;
+        color: black;
+    }
+</style>
